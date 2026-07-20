28030a1eae6435322e4d2f9f71d57bf55990158f
Modhak Kushalappa <PES2UG25EC163@stu.pes.edu>
2026-07-20 22:54:06 +0530

    Refactor sitemap route and add admin route

--- api/app.py
+++ api/app.py
@@ -1,21 +1,6 @@
 from flask import Flask, render_template, Response
 
-@app.route('/sitemap.xml')
-def sitemap():
-    xml = (
-        '<?xml version="1.0" encoding="UTF-8"?>\n'
-        '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
-        '  <url>\n'
-        '    <loc>https://ieee-ras-pesu-ec.vercel.app/</loc>\n'
-        '    <lastmod>2026-07-20</lastmod>\n'
-        '    <changefreq>weekly</changefreq>\n'
-        '    <priority>1.0</priority>\n'
-        '  </url>\n'
-        '</urlset>'
-    )
-    return Response(xml, mimetype='text/xml')
-
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
 
 @app.route('/')
 def home():
@@ -29,14 +14,36 @@ def about():
 def events():
     return render_template('events.html')
 
-@app.route('/team')
-def team():
-    return render_template('team.html')
+@app.route('/admin')
+def admin():
+    return render_template('admin.html')
 
-# The dedicated game page route
-@app.route('/sandbox')
-def sandbox():
-    return render_template('sandbox.html')
+@app.route('/sitemap.xml')
+def sitemap():
+    xml_content = (
+        '<?xml version="1.0" encoding="UTF-8"?>\n'
+        '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">\n'
+        '  <url>\n'
+        '    <loc>https://ieee-ras-pesu-ec.vercel.app/</loc>\n'
+        '    <lastmod>2026-07-20</lastmod>\n'
+        '    <changefreq>weekly</changefreq>\n'
+        '    <priority>1.0</priority>\n'
+        '  </url>\n'
+        '  <url>\n'
+        '    <loc>https://ieee-ras-pesu-ec.vercel.app/about</loc>\n'
+        '    <lastmod>2026-07-20</lastmod>\n'
+        '    <changefreq>monthly</changefreq>\n'
+        '    <priority>0.8</priority>\n'
+        '  </url>\n'
+        '  <url>\n'
+        '    <loc>https://ieee-ras-pesu-ec.vercel.app/events</loc>\n'
+        '    <lastmod>2026-07-20</lastmod>\n'
+        '    <changefreq>weekly</changefreq>\n'
+        '    <priority>0.8</priority>\n'
+        '  </url>\n'
+        '</urlset>'
+    )
+    return Response(xml_content, mimetype='text/xml')
 
 if __name__ == '__main__':
     app.run(debug=True)
