5419ea19edf3d20b7cfea8a26b1c86d029da22fa
Modhak Kushalappa <PES2UG25EC163@stu.pes.edu>
2026-07-23 18:28:43 +0530

    Update app.py

--- api/app.py
+++ api/app.py
@@ -1,18 +1,26 @@
 from flask import Flask, render_template
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='../templates', static_folder='../static')
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
 @app.route('/about')
-def about_page():
+def about():
     return render_template('about.html')
 
+@app.route('/events')
+def events():
+    return render_template('events.html')
+
 @app.route('/team')
-def team_page():
+def team():
     return render_template('team.html')
+
+@app.route('/sandbox')
+def sandbox():
+    return render_template('sandbox.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
